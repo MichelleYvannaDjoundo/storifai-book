c15bb4d834f24377ed89ba19c480bc84679aa4cf
Michelle Djoundo <michelleyvanna3721@gmail.com>
2023-09-19 10:19:09 +0100

    Comptage du nombre de jours successifs

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:ashresume/pages/Auth/register.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/route_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Auth/login_page.dart';
 
@@ -13,6 +14,37 @@ class ProfileScreen1 extends StatefulWidget {
 }
 
 class _ProfileScreen1State extends State<ProfileScreen1> {
+  int successiveDays = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    countSuccessiveDays();
+  }
+
+  Future<void> countSuccessiveDays() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? lastLogin = prefs.getString('last_login');
+
+    DateTime currentDate = DateTime.now();
+    DateTime lastLoginDate = lastLogin != null
+        ? DateTime.parse(lastLogin)
+        : currentDate; // Use the current date as last login if it's the first login
+    bool isSuccessiveDays = currentDate.difference(lastLoginDate).inDays == 1;
+
+    if (isSuccessiveDays) {
+      setState(() {
+        successiveDays++;
+      });
+    } else {
+      setState(() {
+        successiveDays = 0;
+      });
+    }
+
+    await prefs.setString('last_login', currentDate.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +72,12 @@ class _ProfileScreen1State extends State<ProfileScreen1> {
                 ),
               ]),
             ),
-            const SizedBox(height: 16),          
+            const SizedBox(height: 16),
+            Text(
+              'Successive Days: $successiveDays',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -48,7 +85,7 @@ class _ProfileScreen1State extends State<ProfileScreen1> {
                   MaterialPageRoute(builder: (context) => RegistrationScreen()),
                 );
               },
-              child: Text("Please login")
+              child: Text("Please login"),
             ),
           ],
         ),
